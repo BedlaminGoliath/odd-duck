afac67428d960261e9e36cd52a9c6809432ba7fd
christopher lopez <christopherwlopez89@gmail.com>
2022-08-15 21:00:31 -0700

    added my function to generate numbers that will be my crux to assign those numbers to an image and then display that image

--- app.js
+++ app.js
@@ -5,16 +5,15 @@ let maxRound = 26;
 let currentRound = 0;
 let timesViewed = 0;
 
-// create constructor function
-function Product (name, fileExtention='jpg', timesViewed, click){
+// create constructor function that takes in name, filepath and times viewed as arguements
+function Product (name, fileExtention='jpg', click = 0){
   this.name = name;
   this.source = `img/${name}.${fileExtention}`;
   this.timesViewed = 0;
-  this.timesClicked = 0;
   this.click = click;
 
 }
-// Array of all products
+// Array of all products that take in name, file path, and times viewed as parameters
 let allProducts = [
 
   new Product ('bag','./assests/bag.jpg', timesViewed),
@@ -30,8 +29,8 @@ let allProducts = [
   new Product ('pen', '.assets/pen.jpg', timesViewed),
   new Product ('pet-sweep', '.assets/pet-sweep.jpg', timesViewed),
   new Product ('scissors', '.assets/scissors.jpg', timesViewed),
-  new Product ('shark', '.assets/ shark.jpg', timesViewed),
-  new Product ('sweep','png'),
+  new Product ('shark', '.assets/shark.jpg', timesViewed),
+  new Product ('sweep','.assets/sweep.png'),
   new Product ('tauntaun', '.assets/tauntaun.jpg', timesViewed),
   new Product ('unicorn', '.assets/unicorn.jpg', timesViewed),
   new Product ('water-can', '.assets/water-can.jpg', timesViewed),
@@ -40,35 +39,36 @@ let allProducts = [
 
 console.log(allProducts);
 
-// Random Image
+// Random Image that will generate a random image (number)
 
 function randomImage(){
 
   return Math.floor(Math.random () * allProducts.length);
 }
 
-let button = document.getElementById('button');
-button.addEventListener('click', showNewImage);
 
-// create function showNewImage
-function showNewImage(){
 
-  let product = allProducts[randomImage()];
+// let button = document.getElementById('button');
+// button.addEventListener('click', showNewImage);
 
-  let img = document.getElementById('productImage');
-  img.src = `assets/${product.name}.jpg`;
-  img.alt = product.name;
-  img.title = product.name;
-  //   increment shown products
-  product.click++;
-  currentRound++;
-  if (currentRound === maxRound){
-    button.removeEventListener('click', showNewImage);
-  }
-}
-showNewImage();
+// // create function showNewImage
+// function showNewImage(){
 
-// Unique image checker
+//   let product = allProducts[randomImage()];
+
+//   let img = document.getElementById('productImages');
+//   img.src = `assets/${product.name}.jpg`;
+//   img.alt = product.name;
+//   img.title = product.name;
+//   //   increment shown products
+//   product.click++;
+//   currentRound++;
+//   if (currentRound === maxRound){
+//     button.removeEventListener('click', showNewImage);
+//   }
+// }
+// showNewImage();
+
 
 function randomizer () {
   let randomArray = [];
@@ -77,18 +77,34 @@ function randomizer () {
 
     let randomIndex = randomImage();
 
+
     // eslint-disable-next-line no-empty
     if (randomArray.includes(randomIndex)){
     }
-    // do nothing
     else {
       randomArray.push(randomIndex);
     }
 
   }
+  console.log(randomArray);
 }
-
 randomizer();
 
-// let images = document.getElementById('productImage');
-// let
+
+// grab the elements that i need to assign to.
+
+let image = document.getElementById('productImage');
+image.addEventListener('click', buttonrandomizer);
+
+let imageTwo = document.getElementById('productImage2');
+imageTwo.addEventListener('click', buttonrandomizer);
+
+let imageThree = document.getElementById('productImage3');
+imageThree.addEventListener('click', buttonrandomizer);
+
+function buttonrandomizer(){
+
+//   let genNewImage = randomizer();
+//   let 
+
+// }
